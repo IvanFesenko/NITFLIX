3f104fe6790274e9514aa179e0b7c443b771b6dd
Ivan Fesenko <pharmivan@gmail.com>
2020-10-28 12:00:31 +0200

    fonts, packs, scss added

---
+++ src/js/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDuvnT4jggcdrgUHF7AnWYw3zZH5CnwRSA',
+  authDomain: 'nitflix-992cf.firebaseapp.com',
+  databaseURL: 'https://nitflix-992cf.firebaseio.com',
+  projectId: 'nitflix-992cf',
+  storageBucket: 'nitflix-992cf.appspot.com',
+  messagingSenderId: '527944034705',
+  appId: '1:527944034705:web:91a2ae547fba32179cd230',
+});

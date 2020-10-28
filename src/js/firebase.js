41d9e87ed36fe353bd07db932281efe7409aa0ae
Ivan Fesenko <pharmivan@gmail.com>
2020-10-28 18:50:38 +0200

    registration added

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import Refs from './refs';
+import { onLogInState, onLogOutState } from './registration';
 
 firebase.initializeApp({
   apiKey: 'AIzaSyDuvnT4jggcdrgUHF7AnWYw3zZH5CnwRSA',
@@ -11,3 +13,83 @@ firebase.initializeApp({
   messagingSenderId: '527944034705',
   appId: '1:527944034705:web:91a2ae547fba32179cd230',
 });
+
+firebase.auth().onAuthStateChanged(fbUser => {
+  if (fbUser) {
+    onLogInState();
+  } else {
+    onLogOutState();
+  }
+});
+
+async function addUserToDB(user) {
+  try {
+    const { uid, email } = user;
+    const db = firebase.database();
+    const users = db.ref('users');
+    users.child(uid).set({ email: email });
+  } catch {
+    console.error('user add failed');
+  }
+}
+
+async function userExists(user) {
+  try {
+    const { uid } = user;
+    const db = firebase.database();
+    const users = db.ref(`/users/`);
+    const dataSnapshot = await users.once('value');
+    const data = dataSnapshot.val();
+    if (!data.hasOwnProperty(uid)) {
+      await addUserToDB(user);
+    }
+  } catch {
+    alert('User with this e-mail already exists');
+  }
+}
+
+export async function registration() {
+  try {
+    const auth = firebase.auth();
+    const user = await auth.createUserWithEmailAndPassword(
+      Refs.email.value,
+      Refs.password.value,
+    );
+    addUserToDB(user);
+  } catch {
+    alert('User with this e-mail already exists');
+  }
+}
+
+export async function basicAuthorization() {
+  try {
+    const auth = firebase.auth();
+    await auth.signInWithEmailAndPassword(
+      Refs.email.value,
+      Refs.password.value,
+    );
+  } catch {
+    alert('Failed to login');
+  }
+}
+
+export function googleAuthorization() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      userExists(result.user);
+    })
+    .catch(function (error) {
+      console.error(error.message);
+    });
+}
+
+export function logOut() {
+  firebase.auth().signOut();
+}
+
+function getCurrentUser() {
+  return firebase.auth().currentUser;
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,7 @@
 export default {
   trendingContainer: document.querySelector('.js-trending__container'),
+  googleBtn: document.querySelector('#googleIn'),
+  logInBtn: document.querySelector('#loginbtn'),
+  singUpBtn: document.querySelector('#singupbtn'),
+  logOutBtn: document.querySelector('#logOut'),
 };

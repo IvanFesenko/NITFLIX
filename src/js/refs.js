1376de35118e5304ec2be75d50bfb3f691d8248e
Ivan Fesenko <pharmivan@gmail.com>
2020-10-30 15:03:55 +0200

    coments added

--- src/js/refs.js
+++ src/js/refs.js
@@ -4,13 +4,15 @@ export default {
   mainTitle: document.querySelector('.main-title'),
   movieContainer: document.querySelector('.js-movie__container'),
   modalBlurContainer: document.querySelector('.js-blur-container'),
-
+  // user Login and Registrations (Do not change key!)
   googleBtn: document.querySelector('#googleIn'),
   logInBtn: document.querySelector('#loginbtn'),
   singUpBtn: document.querySelector('#singupbtn'),
   logOutBtn: document.querySelector('#logOut'),
   inputEmail: document.querySelector('#inputEmail'),
   inputPassword: document.querySelector('#inputPassword'),
+  //END user Login and Registrations
+
   searchForm: document.querySelector('.search-form'),
   searchFormInput: document.querySelector('.search-form__input'),
 };

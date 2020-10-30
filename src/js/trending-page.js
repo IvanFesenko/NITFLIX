d0e65dd62ad336cd8b535973c00898f4bfa1d225
Maximusvin <Maximusvin@gmail.com>
2020-10-30 11:09:45 +0200

    header

--- src/js/trending-page.js
+++ src/js/trending-page.js
@@ -4,13 +4,14 @@ import screenSize from './services/screenSize';
 
 function generateTrendingList() {
   const size = screenSize();
-  console.log(screenSize());
+
   apiService
     .getTrending()
     .then(({ data }) => data.results)
     .then(res => {
       res = res.map(item => {
-        item.backdrop_path = apiService.makeImagePath(item.backdrop_path, size);
+        console.log(item);
+        item.poster_path = apiService.makeImagePath(item.poster_path, size);
         return item;
       });
       renderMarkup(res);

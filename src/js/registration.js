cc1e08dd746f8c31be99b2ce20eb7272f3381447
Maximusvin <Maximusvin@gmail.com>
2020-10-31 12:24:50 +0200

    added modal registartion

--- src/js/registration.js
+++ src/js/registration.js
@@ -11,6 +11,8 @@ export function onLogInState() {
   Refs.logInBtn.style.display = 'none';
   Refs.singUpBtn.style.display = 'none';
   Refs.logOutBtn.style.display = 'inline';
+  Refs.accInBtn.style.display = 'none';
+  closeModalReg();
 }
 
 export function onLogOutState() {
@@ -18,14 +20,26 @@ export function onLogOutState() {
   Refs.logInBtn.style.display = 'inline';
   Refs.singUpBtn.style.display = 'inline';
   Refs.logOutBtn.style.display = 'none';
+  Refs.accOutBtn.style.display = 'none';
 }
 
 Refs.googleBtn.addEventListener('click', googleOnClick);
 Refs.logOutBtn.addEventListener('click', logOut);
 Refs.singUpBtn.addEventListener('click', registration);
 Refs.logInBtn.addEventListener('click', basicAuthorization);
+Refs.accInBtn.addEventListener('click', openModalReg);
+Refs.modalCloseBtn.addEventListener('click', closeModalReg)
+
 
 function googleOnClick(e) {
   e.preventDefault();
   googleAuthorization();
+}
+
+function openModalReg() {
+  Refs.modalReg.classList.add('modal-active');
+}
+
+function closeModalReg() {
+  Refs.modalReg.classList.remove('modal-active');
 }

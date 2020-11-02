9d0cf27160bab6160c5823d9a8c88f772ae9f443
Bignichok <bignik545@gmail.com>
2020-11-02 23:58:05 +0200

    bush throtlling

--- src/js/mainScrollUp.js
+++ src/js/mainScrollUp.js
@@ -1,27 +1,5 @@
 import debounce from 'lodash-es/debounce';
 
-// const container = document.querySelector('html');
-// console.log(container);
-// const mainScrollUp = document.querySelector('.main-scrollUp');
-
-// const handleScrollUpBtnClick = () => {
-//   container.scrollTop = 0;
-//   console.log('click');
-// };
-
-// const scrollFunction = () => {
-//   if (container.scrollTop > 20) {
-//     mainScrollUp.style = 'opacity:1';
-//     console.log('show');
-//   } else {
-//     mainScrollUp.style = 'opacity:0';
-//     console.log('hide');
-//   }
-// };
-
-// window.addEventListener('scroll', debounce(scrollFunction, 150));
-// mainScrollUp.addEventListener('click', handleScrollUpBtnClick);
-
 const container = document.querySelector('html');
 console.log(container);
 const mainScrollUp = document.querySelector('.main-scrollUp');
@@ -31,18 +9,15 @@ const handleScrollUpBtnClick = () => {
   console.log('click');
 };
 
-const scrollFunction = (container, btn) => {
+const scrollFunction = () => {
   if (container.scrollTop > 20) {
-    btn.style = 'opacity:1';
+    mainScrollUp.style = 'opacity:1';
     console.log('show');
   } else {
-    btn.style = 'opacity:0';
+    mainScrollUp.style = 'opacity:0';
     console.log('hide');
   }
 };
 
-const testHandler = handleScrollUpBtnClick(container);
-const testFunction = scrollFunction(container, mainScrollUp);
-
-window.addEventListener('scroll', debounce(testFunction, 150));
-mainScrollUp.addEventListener('click', testHandler);
+window.addEventListener('scroll', debounce(scrollFunction, 150));
+mainScrollUp.addEventListener('click', handleScrollUpBtnClick);

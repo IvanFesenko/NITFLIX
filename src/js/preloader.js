f23a020c99fad53a172cbb48ee7e693bb2eac20e
Vadym <vad.evlanov@gmail.com>
2020-11-03 21:47:18 +0200

    add preloader

---
+++ src/js/preloader.js
@@ -0,0 +1,15 @@
+function loadData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 6000);
+  });
+}
+
+loadData().then(() => {
+  const body = document.querySelector('.js-blur-container');
+  const preloader = document.querySelector('.logo-nitflix__container');
+
+  preloader.classList.add('hidden');
+  preloader.classList.remove('visible');
+  body.classList.remove('hidden');
+  body.classList.add('visible');
+});

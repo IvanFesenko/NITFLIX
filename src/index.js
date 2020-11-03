8a7b6a974f09ef02ae30d14c6006b7ec55039429
Maximusvin <Maximusvin@gmail.com>
2020-11-03 18:55:12 +0200

    d

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'normalize.css';
-import 'swiper/swiper-bundle.css';
-import './scss/main.scss';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import './scss/main.scss';
+
 
 //main JS
 import './js/script.js';

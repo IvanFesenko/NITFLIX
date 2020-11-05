0b3a1658ce8e43074e85dcf77f071c728dc7d6e4
Ivan Fesenko <pharmivan@gmail.com>
2020-11-05 19:17:02 +0200

    img size func fix

--- src/js/services/screenSize.js
+++ src/js/services/screenSize.js
@@ -2,7 +2,7 @@ function screenSize() {
   const screenSize = document.body.clientWidth;
 
   if (screenSize < 768) {
-    return 3;
+    return 4;
   } else if (screenSize < 1024) {
     return 4;
   } else {
